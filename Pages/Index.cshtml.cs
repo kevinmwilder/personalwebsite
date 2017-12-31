a67badd1c10e63be05831c2cfbb0aa52badfee45
Bill Wilder <codingoutloud@gmail.com>
2017-12-31 11:40:15 -0500

    Generated site and changed about page.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+namespace site.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // future reference: https://stackoverflow.com/a/29718308
+        public void OnGet()
+        {
+            
+            
+          //  var hxeaders = Response.GetTypedHeaders()
+        }
+    }
+}
